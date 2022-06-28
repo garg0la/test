2be6bcbf87a45fcf54ba165958035d0d336f8e3c
garg0la <astrosuka@proton.me>
2022-06-28 14:22:42 -0300

    setup

---
+++ pages/mint.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Header from '../components/Header';
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div>
+    
+
+    <Header />
+     <div className='text-white'>
+        THIS IS THE MINT PAGE
+     </div>
+
+    </div>
+  )
+}
